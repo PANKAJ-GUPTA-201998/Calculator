e0f3bdf272a4219871ba3e585eee5c1155cc0506
Pankaj Gupta <guptapankaj201998@gmail.com>
2023-05-24 14:32:24 +0530

    new

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 let string = "";
 let buttons = document.querySelectorAll('.button');
-console.log(buttons[0])
 Array.from(buttons).forEach((button)=>{
   button.addEventListener('click', (e)=>{
     if(e.target.innerHTML == '='){
       string = eval(string);
       document.querySelector('input').value = string;
     }
-    else if(e.target.innerHTML == 'C'){
+    else if(e.target.innerHTML == 'AC'){
       string = ""
       document.querySelector('input').value = string;
     }
+    else if(e.target.innerHTML =='^'){
+        string=string*string;
+        document.querySelector('input').value=string;
+    }
     else{ 
-    console.log(e.target)
     string = string + e.target.innerHTML;
     document.querySelector('input').value = string;
       }
